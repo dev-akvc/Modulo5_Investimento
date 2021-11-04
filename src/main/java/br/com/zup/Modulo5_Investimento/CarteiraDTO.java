75ea9207fc72901afbd0aece3f972c7a0df45ee3
Anna Karolyne <dev.akvc@gmail.com>
2021-11-04 11:05:09 -0300

    Criar classe CarteiraDTO

---
+++ src/main/java/br/com/zup/Modulo5_Investimento/CarteiraDTO.java
@@ -0,0 +1,69 @@
+package br.com.zup.Modulo5_Investimento;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+public class CarteiraDTO {
+    @Email
+    private String email;
+    private String nome;
+    @CPF(message = "CPF inválido")
+    private String cpf;
+    private double valorPrevisto;
+    @Size(min = 2, message = "Período mínimo de 2 meses para simular")
+    private int periodoDeAplicacaoMeses;
+    private Risco risco;
+
+    public CarteiraDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getValorPrevisto() {
+        return valorPrevisto;
+    }
+
+    public void setValorPrevisto(double valorPrevisto) {
+        this.valorPrevisto = valorPrevisto;
+    }
+
+    public int getPeriodoDeAplicacaoMeses() {
+        return periodoDeAplicacaoMeses;
+    }
+
+    public void setPeriodoDeAplicacaoMeses(int periodoDeAplicacaoMeses) {
+        this.periodoDeAplicacaoMeses = periodoDeAplicacaoMeses;
+    }
+
+    public Risco getRisco() {
+        return risco;
+    }
+
+    public void setRisco(Risco risco) {
+        this.risco = risco;
+    }
+}
--- src/main/java/br/com/zup/Modulo5_Investimento/Risco.java
+++ src/main/java/br/com/zup/Modulo5_Investimento/Risco.java
@@ -1,10 +1,10 @@
 package br.com.zup.Modulo5_Investimento;
 
-public enum Riscos {
+public enum Risco {
     BAIXO(0.05),
     MEDIO(0.15),
     ALTO(0.25);
 
-    Riscos(double v) {
+    Risco(double v) {
     }
 }
