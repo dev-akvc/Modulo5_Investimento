8e4553cfb994f1032718179b402c70a4d56f826a
Anna Karolyne <dev.akvc@gmail.com>
2021-11-04 12:24:50 -0300

    Exibir investimentos na classe InvestimentoController (não testado ainda)

--- src/main/java/br/com/zup/Modulo5_Investimento/investimento/InvestimentoController.java
+++ src/main/java/br/com/zup/Modulo5_Investimento/investimento/InvestimentoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/investimentos")
@@ -11,6 +12,11 @@ public class InvestimentoController {
 
     @Autowired
     private InvestimentoService investimentoService;
+
+    @GetMapping
+    public List<InvestimentoDTO> exibirInvestimentos (){
+        return investimentoService.exibirInvestimentos();
+    }
 
     @PutMapping("/{email}")
     public void cadastrarInvestimento(@RequestBody @Valid  InvestimentoDTO investimentoDTO){
