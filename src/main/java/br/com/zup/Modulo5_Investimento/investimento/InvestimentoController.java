57963288c19774683cf54c60412345eddea05761
Anna Karolyne <dev.akvc@gmail.com>
2021-11-04 11:23:28 -0300

    Iniciar método cadastrarInvestimento

--- src/main/java/br/com/zup/Modulo5_Investimento/investimento/InvestimentoController.java
+++ src/main/java/br/com/zup/Modulo5_Investimento/investimento/InvestimentoController.java
@@ -1,9 +1,20 @@
 package br.com.zup.Modulo5_Investimento.investimento;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/investimentos")
 public class InvestimentoController {
+
+    @Autowired
+    private InvestimentoService investimentoService;
+
+    @PutMapping("/{email}")
+    public InvestimentoDTO cadastrarInvestimento(@PathVariable String email){
+        return null;
+    }
 }
