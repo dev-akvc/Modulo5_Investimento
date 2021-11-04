d536b38caea92b0f3a881d392ca43e1abfe7ac19
Anna Karolyne <dev.akvc@gmail.com>
2021-11-04 10:47:09 -0300

    Primeiro commit

---
+++ src/main/java/br/com/zup/Modulo5_Investimento/Modulo5InvestimentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.Modulo5_Investimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo5InvestimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo5InvestimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/Modulo5_Investimento/Modulo5InvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.Modulo5_Investimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo5InvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
